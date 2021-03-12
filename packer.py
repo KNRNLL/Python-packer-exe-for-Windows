15f73b4276efeac5b241b118a279f4f11a222b3f
Кирилл <41836986+KNRNLL@users.noreply.github.com>
2021-03-13 02:26:44 +0700

    2.0
    Добавил пункт "Запуск от имени Администратора", ускорил процесс сборки...

--- packer.py
+++ packer.py
@@ -9,15 +9,15 @@ from tkinter import filedialog as fd
 import os as os
 import PyInstaller.__main__
 
+root = tk.Tk()
 checkbuttons_id = int()
 object_id = int()
 
 
 class Buttons():
-    '''
-        Класс отвечает за создание и работу кнопок
-        переключателей и прочих управляющих элементов
-    '''
+    """Класс отвечает за создание и работу кнопок, переключателей и прочих \
+    управляющих элементов."""
+
     def __init__(self, root='root'):
         # Корневой фрейм
         self.root = root
@@ -29,11 +29,8 @@ class Buttons():
         self.axle_y = int(25)
 
     def checkbuttons(self, *kwargz):
-        '''
-            На вход принимаем параметры checkbutton
-            Упаковываем и задаем рабочие параметры
-        '''
-        # Счетчик checkbutton
+        """На вход принимаем параметры checkbutton. \
+        Упаковываем и задаем рабочие параметры."""
         global checkbuttons_id, object_id
 
         self.object, self.text = kwargz
@@ -60,6 +57,7 @@ class Buttons():
             self.object.place(x=self.axle_x, y=(self.axle_y * checkbuttons_id))
 
     def check_select(self):
+        """Event select scheckbox."""
         if self.flag_check.get():
             print(self.text)
             if check_onefile.flag_check.get():
@@ -67,19 +65,20 @@ class Buttons():
         else:
             print('Отменено: ' + self.text)
         if check_icon.flag_check.get():
-            if not icon_patch_text.object.get().endswith('.ico'):
+            if not icon_patch_text.object.get().lower().endswith('.ico'):
                 if icon_patch_text.object.get() != '':
                     icon_patch_text.object.delete(0, 'end')
                 icon_patch_text.object.insert(
-                                    0, 'Выберите файл с расширением \'.ico\'')
+                    0, 'Выберите файл с расширением \'.ico\'')
                 print('Выберите файл с расширением \'.ico\'')
                 check_icon.flag_check.set(0)
 
     def objects_id(self, *kwargz):
-        '''
-            На вход принимаем параметры object - прочие элементы а форме
-            Упаковываем и задаем рабочие параметры
-        '''
+        """
+        На вход принимаем параметры object - прочие элементы на форме.
+
+        Упаковываем и задаем рабочие параметры
+        """
         global object_id
         object_id += 1
         self.object, self.text = kwargz
@@ -87,17 +86,13 @@ class Buttons():
         self.object.place(x=self.axle_x, y=300-20*object_id)
 
 
-root = tk.Tk()
-
-
 #############################################################################
 # Функция сборки пректа
 def file_compilled():
-    '''
-        Эта функция содержит в себе допустимые параметры Pyinstaller в порядке
-        их выполнения.
-    '''
+    """Параметры Pyinstaller в порядке их выполнения."""
     py_compiled_list = ['--name=%s' % input_name_file.get()]
+    if check_clean.flag_check.get():
+        py_compiled_list.insert(0, '--clean')
     if check_onefile.flag_check.get():
         py_compiled_list.append('-F')
     if check_onefolder.flag_check.get():
@@ -106,9 +101,9 @@ def file_compilled():
         py_compiled_list.append('-w')
     if check_icon.flag_check.get():
         py_compiled_list.append('-i=%s' % icon_patch_text.object.get())
+    if check_admin.flag_check.get():
+        py_compiled_list.append('--uac-admin')
     py_compiled_list.append(os.path.abspath(input_patch_file.get()))
-    if check_clean.flag_check.get():
-        py_compiled_list.append('--clean')
     PyInstaller.__main__.run(py_compiled_list)
 #############################################################################
 
@@ -135,130 +130,111 @@ pack_patch_file_text = Buttons(root)
 # label с адресом места сборки
 pack_patch_file_label = Buttons(root)
 list_check = [
-              check_noconsole.checkbuttons('check_noconsole',
-                                           'Отключить запуск терминала'),
-              check_onefile.checkbuttons('check_onefile',
-                                         'Сборка в один файл'),
-              check_onefolder.checkbuttons('check_onefolder',
-                                           'Сборка в одну папку'),
-              check_clean.checkbuttons('check_clean',
-                                       'Очистить временные файлы'),
-              check_icon.checkbuttons('check_icon',
-                                      'Задать иконку к файлу'),
-              icon_patch_text.checkbuttons('icon_patch_text', '.....'),
-              icon_patch_label.checkbuttons('icon_text_label',
-                                            'Задать иконку для проекта:'),
-              pack_patch_file_text.checkbuttons('pack_patch_file_text',
-                                                os.getcwd() + "\\dist\\"),
-              pack_patch_file_label.checkbuttons('pack_patch_file_label',
-                                           'Проект будет сохранен по адресу:'),
-                                                                   ]
-# check_admin.checkbuttons('check_admin', 'Требовать права Админитратора'),
+    check_noconsole.checkbuttons('check_noconsole',
+                                 'Отключить запуск терминала'),
+    check_onefile.checkbuttons('check_onefile', 'Сборка в один файл'),
+    check_onefolder.checkbuttons('check_onefolder', 'Сборка в одну папку'),
+    check_clean.checkbuttons('check_clean', 'Очистить временные файлы'),
+    check_icon.checkbuttons('check_icon', 'Задать иконку к файлу'),
+    icon_patch_text.checkbuttons('icon_patch_text', '.....'),
+    icon_patch_label.checkbuttons('icon_text_label',
+                                  'Задать иконку для проекта:'),
+    pack_patch_file_text.checkbuttons('pack_patch_file_text',
+                                      os.getcwd() + "\\dist\\"),
+    pack_patch_file_label.checkbuttons('pack_patch_file_label',
+                                       'Проект будет сохранен по адресу:'),
+    check_admin.checkbuttons('check_admin', 'Требовать права Админитратора'), ]
 
 
+def bt_open_func():
+    """
+    Кнопка открыть рабочий каталог.
 
-def bt_open_func()-> 'open folder':
-    '''
-        Кнопка открыть рабочий каталог
-        Функция проверяет наличие пути, указанного в поле ввода
-        Если адрес недоступен - создает каталог в полном соответствии
-        с введенными данными в текстовом поле
-    '''
-
+    Функция проверяет наличие пути, указанного в поле ввода
+    Если адрес недоступен - создает каталог в полном соответствии
+    с введенными данными в текстовом поле
+    """
     if os.path.isdir(pack_patch_file_text.object.get()):
         os.system('explorer %s' % os.path.abspath(
-                pack_patch_file_text.object.get()))
+            pack_patch_file_text.object.get()))
     else:
         os.mkdir(pack_patch_file_text.object.get())
         os.system('explorer %s' % os.path.abspath(
-                pack_patch_file_text.object.get()))
+            pack_patch_file_text.object.get()))
 
 
 def file_setting():
-    '''
-        Диалоговое окно выбора скрипта Python для компилляции
-        Проверяет, действительно ли выбран файл с расширением .py
+    """
+    Диалоговое окно выбора скрипта Python для сборки.
 
+    Проверяет, действительно ли выбран файл с расширением .py
         ---> Если да, удаляем содержимое текстового поля (input_patch_file),
              если не пустое. Затем записываем туда путь до файла
              В поле с именем файла (input_name_file) -
-                                     автомтически пишем имя выбранного файла
+                                    автомтически пишем имя выбранного файла
              Далее активируем кнопку компилляции (bt_run)
-
         ---> Если выбран файл не с расширением .py, то выводим соответствующее
              сообщение, а так же деактивируем кнопки, если активны
-    '''
-    file_name = fd.askopenfilename()
-    if file_name.endswith('.py'):
-        if input_patch_file.get() != '':
-            input_patch_file.delete(0, 'end')
+    """
+    file_name = fd.askopenfilename(title='Выберите файл для сборки',
+                                   filetypes=[('Python File', '.py'),
+                                              ('Python File', '.pyw')])
+    if file_name:
+        input_patch_file.delete(0, 'end')
         input_patch_file.insert(0, file_name)
-        if input_name_file.get() == '':
-            input_name_file.insert(0, os.path.basename(file_name))
-        if len(bt_run.place_info()) < 1:
-            bt_run.place(x=20, y=125, height=25)
+        if not input_name_file.get():
+            input_name_file.insert(0, os.path.splitext(
+                                   os.path.basename(file_name))[0])
+        bt_run.place(x=20, y=125, height=25)
         return file_name
-    else:
-        if input_patch_file.get() != '':
-            input_patch_file.delete(0, 'end')
-        input_patch_file.insert(0, 'Выберите файл с расширением \'.py\'')
-        if len(bt_run.place_info()) > 0:
-            bt_run.place_forget()
 
 
-# Функция - иконка проекта. Проверка и обработка.
 def icon_patch_open():
-    icon_name = fd.askopenfilename()
-    if icon_name.endswith('.ico'):
-        if icon_patch_text.object.get() != '':
-            icon_patch_text.object.delete(0, 'end')
+    """Функция - иконка проекта. Проверка и обработка."""
+    icon_name = fd.askopenfilename(title='Выберите файл иконки',
+                                   filetypes=[('images', '*.ico')])
+    if icon_name.lower().endswith('.ico'):
+        icon_patch_text.object.delete(0, 'end')
         icon_patch_text.object.insert(0, icon_name)
         return icon_name
-    else:
-        if icon_patch_text.object.get() != '':
-            icon_patch_text.object.delete(0, 'end')
-        icon_patch_text.object.insert(
-                                    0, 'Выберите файл с расширением \'.ico\'')
-        check_icon.flag_check.set(0)
 
-
-##############################################################################
-# Поле ввода - имя файла и путь к нему
-input_patch_file = tk.Entry()
-input_patch_file.place(x=20,  y=25,
-                       width=240, height=25)
-
-name_patch_file = tk.Label(text='Укажите путь к вашему файлу')
-name_patch_file.place(x=20, y=3, width=180)
-
-# Кнопка диалога выбора файла, который будем компиллировать
-bt_patch = tk.Button(text='...', command=file_setting)
-bt_patch.place(x=260, y=25, height=25)
-
-# Поле ввода - имя для компиллируемого проекта
-input_name_file = tk.Entry()
-input_name_file.place(x=20, y=75,
-                      width=240, height=25)
-
-# Кнопка компиллировать файл
-bt_run = tk.Button(text='Запустить сборку!', command=file_compilled)
-
-# Lbel name file
-name_your_file = tk.Label(text='Как вы назовете ваш проект?')
-name_your_file.place(x=20, y=50, width=180)
-##############################################################################
-
-####################################################################
-# Указываем параметры отдельных объектов, если нцужно.
-# Деактивируем текстовое поле указания рабочего каталога
-pack_patch_file_text.object.configure(state='disable')
-# задаем параметры кнопки открытия рабочего каталога
-pack_patch_file_text.object_bt.configure(text='open', command=bt_open_func)
-# Параметры кнопки пути к иконке...
-icon_patch_text.object_bt.configure(text='open', command=icon_patch_open)
 
 if __name__ == '__main__':
+
+    # ###################################################################### #
+    # Поле ввода - имя файла и путь к нему
+    input_patch_file = tk.Entry()
+    input_patch_file.place(x=20,  y=25, width=240, height=25)
+
+    name_patch_file = tk.Label(text='Укажите путь к вашему файлу')
+    name_patch_file.place(x=20, y=3, width=180)
+
+    # Кнопка диалога выбора файла, который будем компиллировать
+    bt_patch = tk.Button(text='...', command=file_setting)
+    bt_patch.place(x=260, y=25, height=25)
+
+    # Поле ввода - имя для проекта
+    input_name_file = tk.Entry()
+    input_name_file.place(x=20, y=75, width=240, height=25)
+
+    # Кнопка "Запустить сборку"
+    bt_run = tk.Button(text='Запустить сборку!', command=file_compilled)
+
+    # Lbel name file
+    name_your_file = tk.Label(text='Как вы назовете ваш проект?')
+    name_your_file.place(x=20, y=50, width=180)
+
+    # ###################################################################### #
+
+    # Указываем параметры отдельных объектов, если нужно.
+    # Деактивируем текстовое поле указания рабочего каталога
+    pack_patch_file_text.object.configure(state='disable')
+    # Кнопка открытия рабочего каталога
+    pack_patch_file_text.object_bt.configure(text='open', command=bt_open_func)
+    # Кнопка пути к иконке...
+    icon_patch_text.object_bt.configure(text='open', command=icon_patch_open)
     root.title("Упаковщик скриптов Python в .exe")
     root.geometry('600x400')
     root.resizable(0, 0)
+    root.mainloop()
     root.mainloop()
